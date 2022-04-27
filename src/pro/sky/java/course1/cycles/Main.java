34cf9707a496a0a7150803a805581e2e9572448f
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2022-04-27 18:15:17 +1000

    домашка циклы. Исправил в соответствии с комментарием.

--- src/pro/sky/java/course1/cycles/Main.java
+++ src/pro/sky/java/course1/cycles/Main.java
@@ -18,23 +18,19 @@ public class Main {
 
         // задание 2
         int firstFriday = 2;
-        for (; firstFriday < 31; firstFriday +=7) {
+        for (; firstFriday <= 31; firstFriday +=7) {
 
             System.out.println("Сегодня пятница " + firstFriday + " число. Необходимо подготовить отчет.");
             }
 
         // задание 3
-        int s = 0;
-        int startPeriod = 1822;
-        int endPeriod = 2122;
-        for (; s <= 2122; s+=79) {
-            if (s >= startPeriod && s <= endPeriod) {
-               System.out.println(s);
+
+        for (int s = 1822; s <= 2122; s+=1)
+            if (s % 79== 0) {
+                System.out.println(s);
 
 
             }
-
-        }
 
 
 
